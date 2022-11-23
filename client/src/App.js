e3418384641cedde82edeac160222fcbca0abb62
Levi Mendyk <levimendyk@Levis-MacBook-Air.local>
2022-11-22 19:40:12 -0700

    Added starter code for a nav bar home and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
