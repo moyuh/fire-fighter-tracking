5b8035852a324615ad1f908b98156ec2a146d014
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-12-01 14:58:07 -0700

    Geolocation works and converts celcius to f

--- client/src/components/WeatherData/index.js
+++ client/src/components/WeatherData/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { toFahrenheit } from '../../utils/toFahrenheit';
 const WeatherData = (props) => {
   console.log(props);
   return (
     <>
       <h2>Forecast Data</h2>
       <ul>
-        <li>Max Temp: {props.searchedWeather.maxTemp.value}</li>
+        <li>
+          Max Temp:{' '}
+          {toFahrenheit(props.searchedWeather.maxTemp.value.toFixed(0))}° F
+        </li>
         <li>
           Relative Humidity: {props.searchedWeather.relativeHumidity.value}%
         </li>
         <li>
-          20ft Winds: {props.searchedWeather.twentyFootWindDirection} at{' '}
-          {props.searchedWeather.twentyFootWindSpeed} mph{' '}
+          20ft Winds:{' '}
+          {props.searchedWeather.twentyFootWindDirection
+            ? `${props.searchedWeather.twentyFootWindDirection} at`
+            : 'Unavailable'}
+          {props.searchedWeather.twentyFootWindSpeed
+            ? props.searchedWeather.twentyFootWindSpeed
+            : ''}
         </li>
-        <li>LAL: {props.searchedWeather.lal.value}</li>
+        <li>
+          LAL:{' '}
+          {props.searchedWeather.lal.value
+            ? props.searchedWeather.lal.value
+            : 'Unavailable'}
+        </li>
         <li>Haines: {props.searchedWeather.hainesIndex.value}</li>
       </ul>
     </>
